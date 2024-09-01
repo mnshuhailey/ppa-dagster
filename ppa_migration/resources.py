53222bbc3bff9ccc6813bcaa09851a471f201491
mnshuhailey <mshuhaileyfx@gmail.com>
2024-09-01 18:25:41 +0800

    refactor: change resource to PPA server

--- ppa_migration/resources.py
+++ ppa_migration/resources.py
@@ -29,10 +29,10 @@ def postgres_db_resource(context):
 def sqlserver_db_resource(context):
     conn_str = (
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=192.168.68.112;"
-        "DATABASE=Test;"
-        "UID=sa;"
-        "PWD=123qwe;"
+        "SERVER=10.10.1.199;"
+        "DATABASE=PPA;"
+        "UID=noor.shuhailey;"
+        "PWD=Lzs.user831;"
         "Trust_Connection=yes;"
     )
     try:
